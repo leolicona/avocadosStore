6e934c6beb8fd727b7b0a74da3773d19197c2155
Leobardo Licona Soto <leobardo.licona@elektra.com.mx>
2023-01-19 09:01:55 -0600

    feat(router): se creo un ejemplo de routing estatico y dinámico

---
+++ pages/product/[productId].js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+
+const { query: { productId}} = useRouter();
+
+ const Product = ({}) => {
+    return ( 
+        <Fragment>
+            
+        </Fragment>
+     );
+ }
+  
+ export default Product;
